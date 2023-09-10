0b4a55aad22eb8d51c5610b4063ca4b28f24b5bb
Rahul Dhiman <122608786+devrahul19@users.noreply.github.com>
2023-09-11 02:05:40 +0530

    morse.ino

--- morse.ino
+++ morse.ino
@@ -1,64 +1,63 @@
-#include <string.h>
+const int buttonPin = 3;
+const int ledPin = 12;
 
-bool isMorseBlinking = false;       // Tracks if the LED is currently blinking in Morse code
-String inputtedName = "";           // Stores the name entered through serial input
-const int BUTTON_PIN = 3;           // Pin for the button input
+int buttonState = LOW;
+int lastButtonState = LOW;
+bool blinking = false;
+const char* name = "Vasudha"; 
+int nameLength = strlen(name);
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Set the built-in LED as an output
-  pinMode(BUTTON_PIN, INPUT_PULLUP);// Set the button pin as an input with internal pull-up resistor
-  Serial.begin(9600);               // Initialize serial communication
-}
-
-int getAlphabetPosition(char letter) {
-  return (letter - 'a');            // Calculate the position of the letter in the alphabet
-}
-
-String morseCodeArray[26]{
-  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
-  ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
-  "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." 
+// Define Morse code for each letter (A-Z)
+const char* morseCode[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+  "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
 };
 
-void blinkMorseLED(String morseCode) {
-  for (int i = 0; i < morseCode.length(); i++) {
-    if (morseCode[i] == '.') {
-      digitalWrite(LED_BUILTIN, HIGH); // Turn on LED for dot
-      delay(1000);                      // Delay for dot duration
-      digitalWrite(LED_BUILTIN, LOW);  // Turn off LED
-      delay(500);                       // Delay between dot and dash
-    } else if (morseCode[i] == '-') {
-      digitalWrite(LED_BUILTIN, HIGH); // Turn on LED for dash
-      delay(2000);                      // Delay for dash duration
-      digitalWrite(LED_BUILTIN, LOW);  // Turn off LED
-      delay(500);                       // Delay between characters
-    }
-  }
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // Use internal pull-up resistor
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    if (!isMorseBlinking) {
-      isMorseBlinking = true;
+  buttonState = digitalRead(buttonPin);
 
-      if (inputtedName == "") {
-        Serial.println("Enter your name");
-        while (!Serial.available()) {
-          // Wait until serial data is available
-        }
-        inputtedName = Serial.readStringUntil('\n'); // Read the name from serial input
-        Serial.println(inputtedName);
-      }
-      
-      for (int i = 0; i < inputtedName.length(); i++) {
-        int alphabetPosition = getAlphabetPosition(inputtedName[i]);
-        String morseCode = morseCodeArray[alphabetPosition];
-        blinkMorseLED(morseCode);        // Blink LED based on Morse code
-        digitalWrite(LED_BUILTIN, LOW); // Turn off LED
-        delay(3000);                     // Delay between letters
+  if (buttonState != lastButtonState) {
+    if (buttonState == LOW) {
+      blinking = !blinking; // Toggle blinking state
+      if (blinking) {
+        blinkName();
+      } else {
+        digitalWrite(ledPin, LOW); // Turn off LED if blinking is stopped
       }
     }
-  } else {
-    isMorseBlinking = false;
+    lastButtonState = buttonState;
   }
+}
+
+void blinkMorseCharacter(const char* morseCharacter) {
+  for (int i = 0; morseCharacter[i] != '\0'; i++) {
+    if (morseCharacter[i] == '.') {
+      digitalWrite(ledPin, HIGH);
+      delay(200); // Dot duration
+    } else if (morseCharacter[i] == '-') {
+      digitalWrite(ledPin, HIGH);
+      delay(600); // Dash duration
+    }
+    digitalWrite(ledPin, LOW);
+    delay(200); // Gap between dots and dashes
+  }
+}
+
+void blinkName() {
+  for (int i = 0; i < nameLength; i++) {
+    char letter = toupper(name[i]);
+    if (letter >= 'A' && letter <= 'Z') {
+      int index = letter - 'A';
+      if (index >= 0 && index < 26) {
+        const char* morse = morseCode[index];
+        blinkMorseCharacter(morse);
+      }
+      delay(600); // Gap between letters
+}
+}
 }
